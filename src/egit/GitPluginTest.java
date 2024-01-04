4ee4d6743454a4d669a485ebf39fdcb0711c9cd7
Darko41 <kracunovicd@icloud.com>
2024-01-04 10:15:01 +0100

    Commiting to new branch bug#1234

--- src/egit/GitPluginTest.java
+++ src/egit/GitPluginTest.java
@@ -2,7 +2,7 @@ package egit;
 
 public class GitPluginTest {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
